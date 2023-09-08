330d80b36eb577a5a43345b9b5084c44a5e829a3
Natsuki Okada <coconana.9042@gmail.com>
2023-09-08 21:22:46 +0900

    WIP css desktop ver

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,24 @@ const jobData = data;
 
 <template>
   <main>
-    <div class="main">
+    <header>
+      <svg width="1440" height="156" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path id="a" d="M0 0h1440v156H0z"/></defs><g fill="none" fill-rule="evenodd"><mask id="b" fill="#fff"><use xlink:href="#a"/></mask><use xlink:href="#a"/><g mask="url(#b)" fill="#63BABA"><path d="M495.73 563.419c-51.439-48.795-53.583-130.05-4.788-181.489a128.377 128.377 0 0 1 4.789-4.789l418.16-396.66c55.045-52.215 141.329-52.215 196.374 0 50.418 47.825 53.478 126.835 7.634 178.39l188.06-178.39c55.045-52.215 141.328-52.215 196.373 0 51.44 48.794 53.583 130.049 4.789 181.488a128.377 128.377 0 0 1-4.789 4.789l-418.158 396.658c-55.045 52.215-141.329 52.215-196.374 0-50.418-47.825-53.478-126.835-7.634-178.39L692.104 563.42c-55.045 52.215-141.328 52.215-196.373 0ZM360.464 45.523c-51.44-48.794-53.583-130.05-4.789-181.488a128.377 128.377 0 0 1 4.789-4.79l418.163-396.663c55.046-52.215 141.33-52.215 196.374 0 51.44 48.794 53.583 130.05 4.789 181.488a128.376 128.376 0 0 1-4.789 4.79L556.837 45.522c-55.045 52.215-141.328 52.215-196.373 0Zm-468.795 71.832c-51.439-48.795-53.583-130.05-4.789-181.489a128.377 128.377 0 0 1 4.79-4.788l418.16-396.661c55.045-52.215 141.328-52.215 196.373 0 51.44 48.794 53.583 130.05 4.79 181.488a128.378 128.378 0 0 1-4.79 4.789l-418.16 396.66c-55.045 52.216-141.329 52.216-196.374 0Z"/></g></g></svg> 
+    </header>
+      <div class="job-list">
+        <div class="grid-flow">
 
-      <div class="main__list">
-
-        <div v-for="job in jobData" :key="job.id" class="flex-group">
+        <div v-for="job in jobData" :key="job.id" class="job-item">
           <img :src=" job.logo" alt="company's logo">
-          <div class="main__items">
+          
+          <div class="flex-group">
 
             <div class="labels">
-              <h1 class="company">{{ job.company }}</h1>
+              <h2 class="company">{{ job.company }}</h2>
               <p class="new-label">NEW!</p>
               <p class="feture-label">FEATURED</p>
             </div>
 
-            <h2 class="position">{{ job.position }}</h2>
+            <h1 class="position">{{ job.position }}</h1>
             
             <div class="category">
               <p class="posted">{{ job.postedAt }}</p>
@@ -32,18 +35,114 @@ const jobData = data;
 
           </div>
 
-          <div class="main__tags">{{ job.languages }}</div>
+          <div class="job-tag">{{ job.languages }}</div>
 
         </div>
-
+        </div>
       </div>
-    </div>
   </main>
   
 </template>
 
 <style>
-.flex-group {
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;700&display=swap');
+
+:root {
+  --clr-primary-darkCyan:hsl(180, 29%, 50%);
+  --clr-background-lightCyan:hsl(180, 52%, 96%);
+  --clr-position-darkCyan:hsl(180, 14%, 20%);
+  --clr-category-darkGray:hsl(180, 8%, 52%);
+  --clr-filterTablets-lightCyan:hsl(180, 31%, 95%);
+
+  --fs-body: 0.93rem;
+  --fs-company: 1.2rem;
+  --fs-position: 1.5rem;
+  --fw-bold: 700;
+  --fw-regular: 500;
+  --font-family: 'League Spartan', sans-serif;
+}
+
+main {
+  font-family: var(--font-family);
+  font-size: var(--fs-body);
+  background-color: var(--clr-background-lightCyan);
+  color: var(--clr-primary-darkCyan);
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+
+header {
+  background-color: var(--clr-primary-darkCyan);
+}
+
+.grid {
+  display: grid;
+  gap: 3rem;
+}
+
+/* .job-list {
+  display: grid;
+  gap: 3rem;
+  max-width: 46rem;
+} */
+
+.grid-flow {
+  display: grid;  
+  align-content: start;
+  gap: 1rem;
+}
+
+.job-item {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: .5rem 1rem 2rem;
+  background-color: var(--clr-background-lightCyan);
+}
+
+.labels {
+  display: flex;
+}
+
+.company {
+  font-size: var(--fs-company);
+  font-weight: var(--fw-bold);
+}
+
+.position {
+  font-size: var(--fs-position);
+  color: var(--clr-position-darkCyan);
+}
+
+.category {
+  display: flex;
+  color: var(--clr-category-darkGray);
+  font-weight: var(--fw-regular);
+}
+
+.job-tag {
+  font-weight: var(--fw-bold);
+}
+
+/* 
+@media (min-width: 600px) {
+  main {
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+  }
+} */
+
+/* @media (min-width: 600px) {
+  .job-item {
+    border-radius: 3rem;
+    box-shadow: .5rem 1rem 3rem;
+}
+} */
+
+/* .job-item {
   display: flex;
 }
 
@@ -57,6 +156,6 @@ const jobData = data;
 
 .main__tags {
   display: flex;
-}
+} */
 
 </style>
